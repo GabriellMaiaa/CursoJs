eeba55c12c847f7555995dc4f5852206190da687
Gabriel Maia <gribeiromaia736@gmail.com>
2022-11-19 12:45:43 -0300

    curso primeiros assuntos

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Olá mundo!!")
+prompt('Qual o seu nome?')
+
+// Comentários - São opções que podemos utilizar para não aparecer certa parte no nosso cód.
+// Variáveis - São valores que tem a funçãode armazenar valores. São os tipo de variáveis
+//Var, let e const
+let nome = 'Pedro';
+console.log(nome);
