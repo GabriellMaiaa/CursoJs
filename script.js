6de81f2f2e70f2b6e3b07f15d13f89c6f7fe4f34
Gabriel Maia <gribeiromaia736@gmail.com>
2022-11-24 21:50:41 -0300

    env

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ console.log("Olá mundo!!")
 prompt('Qual o seu nome?')
 
 // Comentários - São opções que podemos utilizar para não aparecer certa parte no nosso cód.
-// Variáveis - São valores que tem a funçãode armazenar valores. São os tipo de variáveis
+// Variáveis - São valores que tem a função de armazenar valores. São os tipo de variáveis
 //Var, let e const
-let nome = 'Pedro';
-console.log(nome);
+let primeiroNome = 'Gabriel';
+let segundoNome = 'Maia';
+// AS variáveis não podem começar com Números e n pode ter espaço
+console.log(primeiroNome);
+
+/// Constantes - São para qunado você quiser que o valor que você está botando na constante
+// nunca seja mudado.
+const teste = 'teste'
